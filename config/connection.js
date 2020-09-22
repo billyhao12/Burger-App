4b5d72a0dce83fda8a8a40d58de2e167548a69f9
Billy Hao <billyhao12@gmail.com>
2020-09-21 21:00:32 -0700

    Using a .env file to store local connection info.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+// Importing local .env file
+require("dotenv").config();
+
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
 });
 
 if (process.env.JAWSDB_URL) {
