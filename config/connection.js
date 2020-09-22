e866619625d0405360c32f8838ced8a1c7c19ffe
Billy Hao <billyhao12@gmail.com>
2020-09-21 20:38:41 -0700

    Got rid of unnecessary code.

--- config/connection.js
+++ config/connection.js
@@ -11,14 +11,7 @@ var connection = mysql.createConnection({
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-  });
-};
+}
 
 // Make connection.
 connection.connect(function(err) {
